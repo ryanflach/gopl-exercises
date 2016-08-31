bba9299be1d3a1f3ead9fa357c52777f43495104
Ryan Flach <ryanflach@users.noreply.github.com>
2016-08-31 08:49:26 -0600

    Update 1-1

--- Ch_1/1-1.go
+++ Ch_1/1-1.go
@@ -9,8 +9,4 @@ import (
 func main() {
 	// Modify the program to also print os.Args[0]
 	fmt.Println(strings.Join(os.Args[0:], " "))
-	// Modify the program to print the index and value of each arg - 1 per line
-	for idx, arg := range os.Args[1:] {
-		fmt.Println(idx, arg)
-	}
 }
